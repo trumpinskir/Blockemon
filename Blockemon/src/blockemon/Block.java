12b630449af0c9d701112fff03bf86c58dca7125
trumpinskir <trumpinskir@email.arizona.edu>
2017-08-03 09:16:44 -0700

    Add files via upload
    First abstract class of the blockemon world

---
+++ Blockemon/src/blockemon/Block.java
@@ -0,0 +1,32 @@
+package blockemon;
+
+public abstract class Block {
+	
+	int dimX;
+	int dimY;
+	String color;
+	
+	
+	
+	public int getDimX() {
+		return dimX;
+	}
+	public void setDimX(int dimX) {
+		this.dimX = dimX;
+	}
+	public int getDimY() {
+		return dimY;
+	}
+	public void setDimY(int dimY) {
+		this.dimY = dimY;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+	
+}
