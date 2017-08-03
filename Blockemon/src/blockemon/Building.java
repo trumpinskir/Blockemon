f70937f10d7ab19a8d0dea3e0a21955cf4260ede
DKdekes <Eric_Walden@baylor.edu>
2017-08-03 11:00:18 -0700

    Add files via upload
    Preliminary Shrub, World, and Building classes

---
+++ Blockemon/src/blockemon/Building.java
@@ -0,0 +1,28 @@
+package Blocks;
+public class Building extends Block{
+	String color = "A09885";
+	int dimX;
+	int dimY;
+	
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getDimX() {
+		return dimX;
+	}
+	public void setDimX(int dimX) {
+		this.dimX = dimX;
+	}
+	public int getDimY() {
+		return dimY;
+	}
+	public void setDimY(int dimY) {
+		this.dimY = dimY;
+	}
+	
+	
+}
---
+++ Blockemon/src/blockemon/World.java
@@ -0,0 +1,39 @@
+package Blocks;
+
+public class World extends Block {
+	String color = "5C872D";
+	int dimX;
+	int dimY;
+	
+	World(int dimX, int dimY){
+		this.dimX = dimX;
+		this.dimY = dimY;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getDimX() {
+		return dimX;
+	}
+
+	public void setDimX(int dimX) {
+		this.dimX = dimX;
+	}
+
+	public int getDimY() {
+		return dimY;
+	}
+
+	public void setDimY(int dimY) {
+		this.dimY = dimY;
+	}
+	
+	
+	
+}
