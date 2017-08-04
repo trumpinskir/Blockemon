1f1b3da5e49f1f15bfb41d384f3d8373d90f4c5f
DKdekes <Eric_Walden@baylor.edu>
2017-08-04 08:57:58 -0700

    Add files via upload
    new building .java with constructor

--- Blockemon/src/blockemon/Building.java
+++ Blockemon/src/blockemon/Building.java
@@ -1,8 +1,22 @@
 package blockemon;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 public class Building extends Block{
-	String color = "A09885";
+	int colorR;
+	int colorG;
+	int colorB;
+	
 	int dimX;
 	int dimY;
+	
+	Building(int xCoordinate, int yCoordinate, int dimX, int dimY, int R, int G, int B, Graphics2D g){
+		Color color = new Color(R,G,B);
+        g.setColor(color);
+        g.fillRect(xCoordinate,yCoordinate, dimX, dimY);
+        // call 2D array fill method
+	}
 	
 	
 	public String getColor() {
